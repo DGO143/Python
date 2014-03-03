1bfc63925e41c713c5ce12c83810622c0cf71ecf
Luuk Willems <kuukeluukuu@gmail.com>
2014-03-03 17:03:41 +0100

    LICENSING EVERYTHING

--- PygLatin.py
+++ PygLatin.py
@@ -1,9 +1,25 @@
 """
-PygLatin Translator by "Locke" or "ArrowHead" (DGO143).
+This is the PygLatin Translator by "Locke \ ArrowHead" (DGO143).
 
-http://pastebin.com/u/DGO143
+ The PygLatin Translator tranlates a word to Pig Latin. It is based on a tutorial that can be found at www.codeacademy.com
+    Copyright (C) 2014  Luuk Willems
 
-ASCII ART XD
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+http://github.com/DGO143
+
+ASCII ART INCOMING
 
 AAAAA RRRRR RRRRR OOOOO W  W  W
 A   A R   R R   R O   O W  W  W
@@ -31,10 +47,19 @@ V 0.0.1.2
 !!! Python 2.7 !!!
 
 """
+import time
 def first_is_vowel():
     rest = word[1:]
     print rest + first + "ay"
 #Welcome text:
+print "PygLatin Translator Copyright (C) 2014  Luuk Willems
+print
+print "   This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'."
+print "   This is free software, and you are welcome to redistribute it"
+print "   under certain conditions; type `show c' for details."
+print " NOTE: `show w' and `show c' don't work!"
+print 
+time.sleep(1)
 print "Welcome to the English to Pig Latin translator!"
 #Asking for the word that's to be translated, storing it in the variable "original".
 original = raw_input("Enter word to be translated:")
